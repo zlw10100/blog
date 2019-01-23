6e94a71eb6b99482227abb2041cdf3b1fcc8e4b8
zlw10100 <330494152@qq.com>
2019-01-23 10:59:48 +0800

    修改单例模式文本说明、增加结构图

--- design_patterns/singleton/start.py
+++ design_patterns/singleton/start.py
@@ -3,35 +3,33 @@
 # 'author':'zlw'
 
 
-class SingletonMeta(type):
-    def __call__(self, *args, **kwargs):
+"""一个简单的单例模式实现程序
+不论对业务类实例化多少次，均只有一个全局唯一的实例化对象，且初始化也仅执行一次。
+"""
+
+
+class MySingletonMeta(type):
+    def __call__(self, *args, **kwargs):  # 重写元类__call__方法
         if not hasattr(self, '_singleton'):
-            instance = object.__new__(self)
-            self.__init__(instance, *args, **kwargs)
+            instance = object.__new__(self)  # 控制实例化过程
+            self.__init__(instance, *args, **kwargs)  # 控制初始化过程
             setattr(self, '_singleton', instance)
-        return getattr(self, '_singleton')
 
-class Tool(object, metaclass=SingletonMeta):
-    def __init__(self, name, age):
+        # 返回实例化对象
+        instance = getattr(self, '_singleton')
+        return instance
+
+
+class Tool(object, metaclass=MySingletonMeta):
+    def __init__(self, name):
         self.name = name
-        self.age = age
-
-    def use_tools(self):
-        print('工具类的实例化对象只需要全局唯一即可。')
 
 
 if __name__ == '__main__':
-    t1 = Tool('zlw', 23)
-    t2 = Tool('kk', 23)
-    print(t1.name, t2.name)
-    print(id(t1), id(t2))
-    print(t1 is t2)
+    t1 = Tool('网络连接工具')
+    t2 = Tool('网络连接工具')
+    print(t1 is t2)  # True
 
-
-
-
-
-
-
-
-
+    t3 = Tool('打印工具')
+    print(t1 is t2 is t3)  # True
+    print(t3.name)  # 网络连接工具
