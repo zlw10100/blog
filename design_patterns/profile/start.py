6c6450d14a598ecc37cb663ae42936753672e7eb
zlw10100 <330494152@qq.com>
2019-01-23 10:20:22 +0800

    修改原型模式文本说明和代码、增加结构图

--- design_patterns/profile/start.py
+++ design_patterns/profile/start.py
@@ -2,23 +2,32 @@
 # -*- coding: utf-8 -*-
 # 'author':'zlw'
 
-from copy import copy
+
+"""个人简历对象的复制程序
+通过copy而不是多次实例化操作来获得同一个对象的多个副本。
+"""
 
 
-# 原型类，提供clone克隆接口
-class Prototype(object):
+from copy import copy, deepcopy
+
+
+# 原型接口类，提供clone克隆方法
+class PrototypeInterface(object):
     def clone(self):
         raise NotImplementedError('方法必须被实现')
 
 
-class Profile(object):
+class Profile(PrototypeInterface):
     def __init__(self, name, age, gender):
         self.name = name
         self.age = age
         self.gender = gender
 
     def clone(self):
-        return copy(self)
+        """如果只需要对象的属性值，则使用copy即可。
+        若需要对象所有深层引用的其他对象，则应该使用deepcopy。
+        """
+        return deepcopy(self)
 
     def show(self):
         print('个人简历信息:')
@@ -35,5 +44,4 @@ if __name__ == '__main__':
         profile_list.append(profile.clone())
 
     print(profile_list)
-
-
+    # 结果是20份内存地址互不相同的profile对象
